1231694872b1433dc955126d303a8669f9c79472
iamMHasan <abcimhasanbro@gmail.com>
2022-12-30 19:09:37 +0600

    env file added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBk99yns6p2xJ2InX0FMxV2cxJya22xyYY",
-  authDomain: "connect-b68a5.firebaseapp.com",
-  projectId: "connect-b68a5",
-  storageBucket: "connect-b68a5.appspot.com",
-  messagingSenderId: "320682251305",
-  appId: "1:320682251305:web:35aab493795cf20cde7838"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
